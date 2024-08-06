7a32da25926da7b83fff00bc77f9699a262d44c8
Ryujy <63836145+Ryujy@users.noreply.github.com>
2024-08-06 13:09:16 +0900

    Title: 행복한 수열의 개수, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240806/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+
+        for (int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        } // map end
+        
+        int cnt = 0;
+
+        // 가로 확인
+        for (int i=0; i<n; i++){
+            int happyCnt = 1;
+            for (int j=1; j<n; j++){
+                if (map[i][j-1] == map[i][j]){
+                    happyCnt++;
+                } else {
+                    happyCnt = 1;
+                }
+                if (happyCnt >= m){
+                    cnt++;
+                }
+            }
+        }
+
+        // 세로 확인
+        for (int i=0; i<n; i++){
+            int happyCnt = 1;
+            for (int j=1; j<n; j++){
+                if (map[j-1][i] == map[j][i]){
+                    happyCnt++;
+                } else {
+                    happyCnt = 1;
+                }
+            }
+            if (happyCnt >= m){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
