63774263d71d9c7e1dc842ec104497225c374904
Ryujy <63836145+Ryujy@users.noreply.github.com>
2024-08-06 14:11:48 +0900

    Title: 행복한 수열의 개수, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240806/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240806/행복한 수열의 개수/number-of-happy-sequence.java
@@ -12,7 +12,7 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
+        map = new int[n+1][n+1];
 
         for (int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
@@ -27,13 +27,12 @@ public class Main {
         L : for (int i=0; i<n; i++){
             int happyCnt = 0;
             for (int j=0; j<n; j++){
-                if (happyCnt >= m-1){
-                    cnt++;
-                    continue L;
-                }
-                if (j-1 < 0) continue;
-                if (map[i][j-1] == map[i][j]){
+                if (map[i][j+1] == map[i][j]){
                     happyCnt++;
+                    if (happyCnt >= m-1){
+                        cnt++;
+                        continue L;
+                    }
                 } else {
                     happyCnt = 0;
                 }
@@ -44,15 +43,14 @@ public class Main {
         L : for (int j=0; j<n; j++){
             int happyCnt = 0;
             for (int i=0; i<n; i++){
-                if (happyCnt >= m-1){
-                    cnt++;
-                    continue L;
-                }
-                if (i-1 < 0) continue;
-                if (map[i-1][j] == map[i][j]){
+                if (map[i+1][j] == map[i][j]){
                     happyCnt++;
                 } else {
                     happyCnt = 0;
+                }
+                if (happyCnt >= m-1){
+                    cnt++;
+                    continue L;
                 }
             }
         }
